b3554e2c228c492b3f9bd5a8394007b962ece13c
Feernandooff <ferobregon03@gmail.com>
2022-04-25 16:21:45 +0100

    finished layout and added local messages

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,16 @@ import styles from './Header.module.css';
 type Props = {};
 
 function Header({}: Props) {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav className={styles.nav}>
+        <a>Discover</a>
+        <a>Browse</a>
+        <a>About</a>
+      </nav>
+      <div className={styles.avatar}></div>
+    </div>
+  );
 }
 
 export default Header;
--- components/MeetingMessages/index.tsx
+++ components/MeetingMessages/index.tsx
@@ -1,38 +1,22 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import styles from './MeetingMessages.module.css';
 
-type Props = {};
-
-function MeetingMessages({}: Props) {
+function MeetingMessages({}) {
+  const [messages, setMessages] = useState([]);
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setMessages([...messages, e.target[0].value]);
+    e.target[0].value = '';
+  };
   return (
     <div className={styles.messageContainer}>
       <div className={styles.messages}>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>peerro</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>Hi</p>
-        <p>peerro</p>
-        <p>peerro</p>
-        <p>peerro</p>
+        {messages.map((m, i) => (
+          <p key={i}>{m}</p>
+        ))}
       </div>
       <div className="input">
-        <form className={styles.formulary}>
+        <form className={styles.formulary} onSubmit={handleSubmit}>
           <input type="text" placeholder="Join the conversation" />
         </form>
       </div>
