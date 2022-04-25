69d0ab147ce85b3dbbb28ad25a00e80938dea32d
Feernandooff <ferobregon03@gmail.com>
2022-04-25 16:07:34 +0100

    Styles added

--- components/MeetingMessages/index.tsx
+++ components/MeetingMessages/index.tsx
@@ -4,7 +4,40 @@ import styles from './MeetingMessages.module.css';
 type Props = {};
 
 function MeetingMessages({}: Props) {
-  return <div className={styles.messageContainer}>MeetingMessages</div>;
+  return (
+    <div className={styles.messageContainer}>
+      <div className={styles.messages}>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>peerro</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>Hi</p>
+        <p>peerro</p>
+        <p>peerro</p>
+        <p>peerro</p>
+      </div>
+      <div className="input">
+        <form className={styles.formulary}>
+          <input type="text" placeholder="Join the conversation" />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default MeetingMessages;
